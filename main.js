24265c8a7cd4ddb960ff5d252f764ee50ee45cf9
Tomasz Sienkiewicz <tomasz.sienkiewicz@mail.com>
2019-06-13 21:09:50 +0200

    add JS technology ;)

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 console.log('siema');
 const name ='Tomek';
-const age = 31;
+const age = 43;
 console.log(name);
 console.log(age);
-console.log(`Nazywam się ${name} + i mam ${age} lat`);
+console.log(`Nazywam się ${name} i mam ${age} lata`);
 
 console.log(`Witaj przybyszu`);
+
+const paragraph = document.querySelector('.paragraph__js');
+
+paragraph.innerHTML = `Nazywam się ${name} i mam ${age} lata`;
