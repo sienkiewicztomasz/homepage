477e41055e84c051c94a08f5b1c261375e8c9ca1
Tomasz Sienkiewicz <tomasz.sienkiewicz@mail.com>
2019-07-03 20:56:05 +0200

    add hamburger to my homepage

--- main.js
+++ main.js
@@ -16,3 +16,20 @@ const welcome = (age,name) => {
 console.log(welcome(43, 'Tomek'));
 
 paragraph.innerHTML = `Nazywam się ${name} i mam ${age} lata`;
+
+const button = document.querySelector('.header__button--js');
+console.log(button);
+
+button.addEventListener('click', (e) => {
+    const header = document.querySelector('.header__title--js');
+    header.innerHTML = 'Hurra, zmieniłem się - nagłówek';
+    header.classList.toggle('header__title--red');
+    alert('Hello');
+});
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
